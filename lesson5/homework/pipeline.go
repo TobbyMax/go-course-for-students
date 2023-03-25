22db37a15ce32ef22ae39abb86ec224c29eac267
TobbyMax <ageev.maxim2003@gmail.com>
2023-03-25 14:49:30 +0300

    hw5: pass 3 tests

--- lesson5/homework/pipeline.go
+++ lesson5/homework/pipeline.go
@@ -11,7 +11,30 @@ type (
 
 type Stage func(in In) (out Out)
 
+func checkContext(ctx context.Context, in In) Out {
+	out := make(chan any)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case val, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- val
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
-	// TODO
-	return nil
+	out := in
+	for _, stage := range stages {
+		out = stage(out)
+	}
+	res := checkContext(ctx, out)
+	return res
 }
