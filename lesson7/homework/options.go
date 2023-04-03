a8ad4a6ba6ea6967a122afed8fa432c590913771
TobbyMax <ageev.maxim2003@gmail.com>
2023-04-03 15:04:13 +0300

    hw7: refactor

---
+++ lesson7/homework/options.go
@@ -0,0 +1,76 @@
+package homework
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+type Options struct {
+	Numeric map[string]int
+	InStr   []string
+	InInt   []int
+}
+
+func (*Options) getOption(str string, opt string) (string, error) {
+	var s string
+	_, err := fmt.Sscanf(str, opt+":%s", &s)
+	if err != nil {
+		return "", ErrInvalidValidatorSyntax
+	}
+	return s, nil
+}
+
+func (o *Options) getOptionNum(str string, opt string) error {
+	if strings.Contains(str, opt) {
+		s, err := o.getOption(str, opt)
+		if err != nil {
+			return ErrInvalidValidatorSyntax
+		}
+		n, err := strconv.Atoi(s)
+		if err != nil {
+			return ErrInvalidValidatorSyntax
+		}
+		o.Numeric[opt] = n
+	}
+	return nil
+}
+
+func (o *Options) parseInOption(t any, st string) error {
+	if strings.Contains(st, "in") && !strings.Contains(st, "min") {
+		s, err := o.getOption(st, "in")
+		if err != nil {
+			return ErrInvalidValidatorSyntax
+		}
+		o.InStr = strings.Split(s, ",")
+		if reflect.TypeOf(t).Kind() == reflect.Int {
+			o.InInt = make([]int, len(o.InStr))
+			for i, v := range o.InStr {
+				n, err := strconv.Atoi(v)
+				if err != nil {
+					return ErrInvalidValidatorSyntax
+				}
+				o.InInt[i] = n
+			}
+		}
+	}
+	return nil
+}
+
+func ParseOptions[T int | string](st string) (Options, error) {
+	numerical := []string{"min", "max", "len"}
+	opts := Options{Numeric: make(map[string]int)}
+	for _, opt := range numerical {
+		err := opts.getOptionNum(st, opt)
+		if err != nil {
+			return Options{}, err
+		}
+	}
+	var t T
+	err := opts.parseInOption(t, st)
+	if err != nil {
+		return Options{}, err
+	}
+	return opts, nil
+}
--- lesson7/homework/validator.go
+++ lesson7/homework/validator.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"github.com/pkg/errors"
 	"reflect"
-	"strconv"
-	"strings"
 )
 
 var ErrNotStruct = errors.New("wrong argument given, should be a struct")
@@ -19,7 +17,6 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	// TODO: implement this
 	res := ""
 	for i, ve := range v {
 		res = res + fmt.Sprint(ve.Err)
@@ -30,135 +27,8 @@ func (v ValidationErrors) Error() string {
 	return res
 }
 
-//func validateSlice(v any, v ValidationErrors) {
-//	switch t.Field(i).Type.Elem().Kind(){
-//	case reflect.String:
-//		validateString()
-//	case reflect.Int:
-//		validateInt()
-//	}
-//}
-
-func validateString(val reflect.Value, st string, ve *ValidationErrors) error {
-	str := val.String()
-	if strings.Contains(st, "min") {
-		var s string
-		_, err := fmt.Sscanf(st, "min:%s", &s)
-		if err != nil {
-			return ErrInvalidValidatorSyntax
-		}
-		min, err := strconv.Atoi(s)
-		if err != nil {
-			return ErrInvalidValidatorSyntax
-		}
-
-		if len(str) < min {
-			*ve = append(*ve, ValidationError{errors.New("field not valid")})
-		}
-	}
-	if strings.Contains(st, "max") {
-		var s string
-		_, err := fmt.Sscanf(st, "max:%s", &s)
-		if err != nil {
-			return ErrInvalidValidatorSyntax
-		}
-		max, err := strconv.Atoi(s)
-		if err != nil {
-			return ErrInvalidValidatorSyntax
-		}
-		if len(str) > max {
-			*ve = append(*ve, ValidationError{errors.New("field not valid")})
-		}
-	}
-	if strings.Contains(st, "len") {
-		var s string
-		_, err := fmt.Sscanf(st, "len:%s", &s)
-		if err != nil {
-			return ErrInvalidValidatorSyntax
-		}
-		l, err := strconv.Atoi(s)
-		if err != nil {
-			return ErrInvalidValidatorSyntax
-		}
-		if len(str) != l {
-			*ve = append(*ve, ValidationError{errors.New("field not valid")})
-		}
-	}
-	if strings.Contains(st, "in") && !strings.Contains(st, "min") {
-		var s string
-		_, err := fmt.Sscanf(st, "in:%s", &s)
-		if err != nil {
-			return ErrInvalidValidatorSyntax
-		}
-		set := strings.Split(s, ",")
-		for i, v := range set {
-			set[i] = v
-		}
-		if !contains(set, str) {
-			*ve = append(*ve, ValidationError{errors.New("field not valid")})
-		}
-	}
-	return nil
-}
-
-func validateInt(val reflect.Value, st string, ve *ValidationErrors) error {
-	num := val.Int()
-	if strings.Contains(st, "min") {
-		var s string
-		_, err := fmt.Sscanf(st, "min:%s", &s)
-		if err != nil {
-			return ErrInvalidValidatorSyntax
-		}
-		min, err := strconv.Atoi(s)
-		if err != nil {
-			return ErrInvalidValidatorSyntax
-		}
-		if num < int64(min) {
-			*ve = append(*ve, ValidationError{errors.New("field not valid")})
-		}
-	}
-	if strings.Contains(st, "max") {
-		var s string
-		_, err := fmt.Sscanf(st, "max:%s", &s)
-		if err != nil {
-			return ErrInvalidValidatorSyntax
-		}
-		max, err := strconv.Atoi(s)
-		if err != nil {
-			return ErrInvalidValidatorSyntax
-		}
-		if num > int64(max) {
-			*ve = append(*ve, ValidationError{errors.New("field not valid")})
-		}
-	}
-	if strings.Contains(st, "len") {
-		l := 0
-		_, err := fmt.Sscanf(st, "len:%d", &l)
-		if err != nil {
-			return ErrInvalidValidatorSyntax
-		}
-		*ve = append(*ve, ValidationError{errors.New("cannot use len-validation for int value")})
-	}
-	if strings.Contains(st, "in") && !strings.Contains(st, "min") {
-		var s string
-		_, err := fmt.Sscanf(st, "in:%s", &s)
-		if err != nil {
-			return ErrInvalidValidatorSyntax
-		}
-		set := strings.Split(s, ",")
-		iSet := make([]int, len(set))
-		for i, v := range set {
-			n, err := strconv.Atoi(v)
-			if err != nil {
-				return ErrInvalidValidatorSyntax
-			}
-			iSet[i] = n
-		}
-		if !contains(iSet, int(num)) {
-			*ve = append(*ve, ValidationError{errors.New("field not valid")})
-		}
-	}
-	return nil
+type Validator struct {
+	Errors ValidationErrors
 }
 
 func contains[T comparable](set []T, val T) bool {
@@ -170,40 +40,76 @@ func contains[T comparable](set []T, val T) bool {
 	return false
 }
 
+func (v *Validator) ValidateField(sf reflect.StructField, val reflect.Value) ValidationErrors {
+	if sf.Tag.Get("validate") == "" {
+		return nil
+	}
+	if !sf.IsExported() {
+		return append(ValidationErrors{}, ValidationError{ErrValidateForUnexportedFields})
+	}
+	switch sf.Type.Kind() {
+	case reflect.String:
+		opts, err := ParseOptions[string](sf.Tag.Get("validate"))
+		if err != nil {
+			return append(ValidationErrors{}, ValidationError{err})
+		}
+		v.validateValue(val, opts)
+	case reflect.Int:
+		opts, err := ParseOptions[int](sf.Tag.Get("validate"))
+		if err != nil {
+			return append(ValidationErrors{}, ValidationError{err})
+		}
+		v.validateValue(val, opts)
+		//case reflect.Slice:
+		//	err := v.validateSlice(sf, val)
+	}
+	return nil
+}
+
+//func (v* Validator) validateSlice(sf reflect.StructField, val reflect.Value) error {
+//	switch sf.Type.Kind() {}
+//}
+
+func (v *Validator) validateValue(val reflect.Value, opts Options) {
+	var n int
+	if val.Kind() == reflect.Int {
+		n = int(val.Int())
+		if opts.InInt != nil && !contains(opts.InInt, n) {
+			v.Errors = append(v.Errors, ValidationError{errors.New("field not valid")})
+		}
+	} else if val.Kind() == reflect.String {
+		n = len(val.String())
+		if opts.InStr != nil && !contains(opts.InStr, val.String()) {
+			v.Errors = append(v.Errors, ValidationError{errors.New("field not valid")})
+		}
+	}
+	for k, l := range opts.Numeric {
+		switch {
+		case k == "min" && n < l:
+			v.Errors = append(v.Errors, ValidationError{errors.New("field not valid")})
+		case k == "max" && n > l:
+			v.Errors = append(v.Errors, ValidationError{errors.New("field not valid")})
+		case k == "len" && n != l:
+			v.Errors = append(v.Errors, ValidationError{errors.New("field not valid")})
+		}
+	}
+}
+
 func Validate(v any) error {
-	// TODO: implement this
 	t := reflect.TypeOf(v)
 	val := reflect.ValueOf(v)
-
-	ve := ValidationErrors{}
+	vld := Validator{}
 	if t.Kind() != reflect.Struct {
 		return ErrNotStruct
 	}
-
 	for i := 0; i < t.NumField(); i++ {
-		if t.Field(i).Tag.Get("validate") == "" {
-			continue
-		}
-		if !t.Field(i).IsExported() {
-			return append(ValidationErrors{}, ValidationError{ErrValidateForUnexportedFields})
-		}
-		switch t.Field(i).Type.Kind() {
-		case reflect.String:
-			err := validateString(val.Field(i), t.Field(i).Tag.Get("validate"), &ve)
-			if err != nil {
-				ve = append(ve, ValidationError{err})
-			}
-		case reflect.Int:
-			err := validateInt(val.Field(i), t.Field(i).Tag.Get("validate"), &ve)
-			if err != nil {
-				ve = append(ve, ValidationError{err})
-			}
-			//case reflect.Slice:
-			//	validateSlice();
+		err := vld.ValidateField(t.Field(i), val.Field(i))
+		if err != nil {
+			return err
 		}
 	}
-	if len(ve) != 0 {
-		return ve
+	if len(vld.Errors) != 0 {
+		return vld.Errors
 	}
 	return nil
 }
