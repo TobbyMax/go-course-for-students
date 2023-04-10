78fa5946ec422270eb70f05b185e8fca770a0728
TobbyMax <ageev.maxim2003@gmail.com>
2023-04-10 13:21:25 +0300

    hw6: clean arch and clear error handling

--- lesson6/homework/internal/ads/ads.go
+++ lesson6/homework/internal/ads/ads.go
@@ -2,8 +2,8 @@ package ads
 
 type Ad struct {
 	ID        int64
-	Title     string
-	Text      string
+	Title     string `validate:"min:1; max:99"`
+	Text      string `validate:"min:1; max:499"`
 	AuthorID  int64
 	Published bool
 }
