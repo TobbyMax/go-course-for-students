73bf5722b08e52fcab074ab57d6734fc076b6c4e
TobbyMax <ageev.maxim2003@gmail.com>
2023-04-13 12:25:35 +0300

    hw8: gin server and  pass basic tests

--- lesson8/homework/cmd/main/main.go
+++ lesson8/homework/cmd/main/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"homework6/internal/adapters/adrepo"
-	"homework6/internal/app"
-	"homework6/internal/ports/httpfiber"
+	"homework8/internal/adapters/adrepo"
+	"homework8/internal/app"
+	"homework8/internal/ports/httpgin"
 )
 
 func main() {
-	server := httpfiber.NewHTTPServer(":18080", app.NewApp(adrepo.New()))
+	server := httpgin.NewHTTPServer(":18080", app.NewApp(adrepo.New()))
 	err := server.Listen()
 	if err != nil {
 		panic(err)
--- lesson8/homework/internal/app/app.go
+++ lesson8/homework/internal/app/app.go
@@ -3,9 +3,7 @@ package app
 import (
 	"context"
 	"fmt"
-
 	"github.com/TobbyMax/validator"
-
 	"homework8/internal/ads"
 )
 
@@ -15,6 +13,12 @@ type App interface {
 	CreateAd(ctx context.Context, title string, text string, uid int64) (*ads.Ad, error)
 	ChangeAdStatus(ctx context.Context, id int64, uid int64, published bool) (*ads.Ad, error)
 	UpdateAd(ctx context.Context, id int64, uid int64, title string, text string) (*ads.Ad, error)
+	GetAd(ctx context.Context, id int64) (*ads.Ad, error)
+	ListAds(ctx context.Context) (*ads.AdList, error)
+
+	//CreateUser(ctx context.Context, nickname string, email string) (*user.User, error)
+	//GetUser(ctx context.Context, id int64) (*user.User, error)
+	//UpdateUser(ctx context.Context, id int64, nickname string, email string) (*user.User, error)
 }
 
 type Repository interface {
@@ -22,6 +26,11 @@ type Repository interface {
 	GetAdByID(ctx context.Context, id int64) (*ads.Ad, error)
 	UpdateAdStatus(ctx context.Context, id int64, published bool) error
 	UpdateAdContent(ctx context.Context, id int64, title string, text string) error
+	GetPublishedAds(ctx context.Context) (*ads.AdList, error)
+
+	//AddUser(ctx context.Context, u user.User) (int64, error)
+	//GetUserByID(ctx context.Context, id int64) (*user.User, error)
+	//UpdateUser(ctx context.Context, id int64, nickname string, email string) error
 }
 
 type AdApp struct {
@@ -50,6 +59,15 @@ func (a AdApp) CreateAd(ctx context.Context, title string, text string, uid int6
 	ad.ID = id
 
 	return &ad, nil
+}
+
+func (a AdApp) GetAd(ctx context.Context, id int64) (*ads.Ad, error) {
+	ad, err := a.repository.GetAdByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return ad, nil
 }
 
 func (a AdApp) ChangeAdStatus(ctx context.Context, id int64, uid int64, published bool) (*ads.Ad, error) {
@@ -94,3 +112,29 @@ func (a AdApp) UpdateAd(ctx context.Context, id int64, uid int64, title string, 
 
 	return ad, nil
 }
+
+func (a AdApp) ListAds(ctx context.Context) (*ads.AdList, error) {
+	al, err := a.repository.GetPublishedAds(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return al, nil
+}
+
+//func (a AdApp) CreateUser(ctx context.Context, nickname string, email string) (*user.User, error) {
+//	u := user.User{Nickname: nickname, Email: email}
+//
+//	if err := validator.Validate(u); err != nil {
+//		return nil, err
+//	}
+//
+//	id, err := a.repository.AddUser(ctx, u)
+//	if err != nil {
+//		return nil, err
+//	}
+//	u.ID = id
+//
+//	return &u, nil
+//}
