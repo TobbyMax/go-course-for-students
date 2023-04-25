8a488be1a9d91c5fd34762ccb0b5b680ba3742ac
TobbyMax <ageev.maxim2003@gmail.com>
2023-04-25 18:25:00 +0300

    hw9: copy hw8 into hw9

---
+++ lesson9/homework/internal/ports/grpc/server.go
@@ -0,0 +1,39 @@
+package grpc
+
+//
+//import (
+//	"github.com/gin-gonic/gin"
+//	"net/http"
+//
+//	"homework9/internal/app"
+//)
+//
+//type Server struct {
+//	port string
+//	app  *gin.Engine
+//}
+//
+//func NewHTTPServer(port string, a app.App) Server {
+//	gin.SetMode(gin.ReleaseMode)
+//	s := Server{port: port, app: gin.New()}
+//
+//	// todo: add your own logic
+//	api := s.app.Group("/api/v1")
+//
+//	// MiddleWare для логирования и паник
+//	api.Use(gin.Logger())
+//	api.Use(gin.Recovery())
+//
+//	api.Use(LogMiddleWare)
+//
+//	AppRouter(api, a)
+//	return s
+//}
+//
+//func (s *Server) Listen() error {
+//	return s.app.Run(s.port)
+//}
+//
+//func (s *Server) Handler() http.Handler {
+//	return s.app
+//}
