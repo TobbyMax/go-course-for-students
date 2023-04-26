454b8d38237e1c29b6d499c232d00ba6fd9d0472
TobbyMax <ageev.maxim2003@gmail.com>
2023-04-26 20:48:18 +0300

    hw9: add gin server

---
+++ lesson9/homework/internal/ports/httpgin/server.go
@@ -0,0 +1,54 @@
+package httpgin
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"homework9/internal/app"
+)
+
+type Server struct {
+	port string
+	app  *gin.Engine
+}
+
+func LogMiddleWare(c *gin.Context) {
+	t := time.Now()
+
+	log.Printf("-- received request -- | method: %s | path: %s\n", c.Request.Method, c.Request.URL.Path)
+
+	c.Next()
+
+	latency := time.Since(t)
+	status := c.Writer.Status()
+
+	log.Printf("-- handled request -- | status: %d | latency: %+v | method: %s | path: %s\n", status, latency, c.Request.Method, c.Request.URL.Path)
+}
+
+func NewHTTPServer(port string, a app.App) Server {
+	gin.SetMode(gin.ReleaseMode)
+	s := Server{port: port, app: gin.New()}
+
+	// todo: add your own logic
+	api := s.app.Group("/api/v1")
+
+	// MiddleWare для логирования и паник
+	api.Use(gin.Logger())
+	api.Use(gin.Recovery())
+
+	api.Use(LogMiddleWare)
+
+	AppRouter(api, a)
+	return s
+}
+
+func (s *Server) Listen() error {
+	return s.app.Run(s.port)
+}
+
+func (s *Server) Handler() http.Handler {
+	return s.app
+}
