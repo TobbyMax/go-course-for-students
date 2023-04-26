36dd454949e625f1a3cf6a50301538bd8ea76d34
TobbyMax <ageev.maxim2003@gmail.com>
2023-04-26 21:25:38 +0300

    hw9: add delete methods to gin server

--- lesson9/homework/internal/ports/httpgin/router.go
+++ lesson9/homework/internal/ports/httpgin/router.go
@@ -10,10 +10,12 @@ func AppRouter(r *gin.RouterGroup, a app.App) {
 	r.PUT("/ads/:ad_id/status", changeAdStatus(a)) // Метод для изменения статуса объявления (опубликовано - Published = true или снято с публикации Published = false)
 	r.PUT("/ads/:ad_id", updateAd(a))              // Метод для обновления текста(Text) или заголовка(Title) объявления
 	r.GET("/ads/:ad_id", getAd(a))                 // Метод для получения объявления по ID
+	r.DELETE("/ads", deleteAd(a))
 
 	r.GET("/ads", listAds(a)) // Метод для получения списка объявлений с фильтрами (по published, userID, date, title)
 
 	r.POST("/users", createUser(a))         // Метод для создания пользователя (user)
 	r.GET("/users/:user_id", getUser(a))    // Метод для получения пользователя по ID
 	r.PUT("/users/:user_id", updateUser(a)) // Метод для обновления имени(Nickname) или почты(Email) пользователя
+	r.DELETE("/users/:user_id", deleteUser(a))
 }
