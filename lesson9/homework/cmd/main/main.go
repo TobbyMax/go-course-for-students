f20f6e005c4591feafc43691d1e0ee856131addc
TobbyMax <ageev.maxim2003@gmail.com>
2023-04-27 00:35:41 +0300

    hw9: parallel working http & grpc

--- lesson9/homework/cmd/main/main.go
+++ lesson9/homework/cmd/main/main.go
@@ -1,15 +1,49 @@
 package main
 
 import (
+	"google.golang.org/grpc"
 	"homework9/internal/adapters/adrepo"
 	"homework9/internal/app"
+	grpcSvc "homework9/internal/ports/grpc"
 	"homework9/internal/ports/httpgin"
+	"log"
+	"net"
+	"sync"
+)
+
+const (
+	grpcPort = ":8080"
+	httpPort = ":18080"
 )
 
 func main() {
-	server := httpgin.NewHTTPServer(":18080", app.NewApp(adrepo.New()))
-	err := server.ListenAndServe()
+	appSvc := app.NewApp(adrepo.New())
+
+	lis, err := net.Listen("tcp", grpcPort)
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to listen: %v", err)
 	}
+
+	svc := grpcSvc.NewService(appSvc)
+	grpcServer := grpc.NewServer()
+	grpcSvc.RegisterAdServiceServer(grpcServer, svc)
+
+	server := httpgin.NewHTTPServer(httpPort, appSvc)
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		err = server.ListenAndServe()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		if err = grpcServer.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+	wg.Wait()
 }
