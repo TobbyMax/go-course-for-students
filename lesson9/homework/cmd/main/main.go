b6f221a57d481ac61969c09deffd2cebfe94fb9b
TobbyMax <ageev.maxim2003@gmail.com>
2023-04-27 19:22:35 +0300

    hw9: refactor graceful shutdown

--- lesson9/homework/cmd/main/main.go
+++ lesson9/homework/cmd/main/main.go
@@ -2,22 +2,16 @@ package main
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"homework9/internal/adapters/adrepo"
 	"homework9/internal/app"
+	"homework9/internal/graceful"
 	grpcSvc "homework9/internal/ports/grpc"
 	"homework9/internal/ports/httpgin"
 
 	"log"
 	"net"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 )
 
 const (
@@ -44,83 +38,14 @@ func main() {
 
 	eg, ctx := errgroup.WithContext(context.Background())
 
-	sigQuit := make(chan os.Signal, 1)
-	signal.Ignore(syscall.SIGHUP, syscall.SIGPIPE)
-	signal.Notify(sigQuit, syscall.SIGINT, syscall.SIGTERM)
-
-	eg.Go(func() error {
-		select {
-		case s := <-sigQuit:
-			log.Printf("captured signal: %v\n", s)
-			return fmt.Errorf("captured signal: %v", s)
-		case <-ctx.Done():
-			return nil
-		}
-	})
-
+	eg.Go(graceful.CaptureSignal(ctx))
 	// run grpc server
-	eg.Go(func() error {
-		log.Printf("starting grpc server, listening on %s\n", grpcPort)
-		defer log.Printf("close grpc server listening on %s\n", grpcPort)
-
-		errCh := make(chan error)
-
-		defer func() {
-			grpcServer.GracefulStop()
-			_ = lis.Close()
-
-			close(errCh)
-		}()
-
-		go func() {
-			if err := grpcServer.Serve(lis); err != nil {
-				errCh <- err
-			}
-		}()
-
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case err := <-errCh:
-			return fmt.Errorf("grpc server can't listen and serve requests: %w", err)
-		}
-	})
-
+	eg.Go(grpcSvc.RunGRPCServerGracefully(ctx, lis, grpcServer))
 	// run http server
-	eg.Go(func() error {
-		log.Printf("starting http server, listening on %s\n", httpServer.Addr)
-		defer log.Printf("close http server listening on %s\n", httpServer.Addr)
-
-		errCh := make(chan error)
-
-		defer func() {
-			shCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-			defer cancel()
-
-			if err := httpServer.Shutdown(shCtx); err != nil {
-				log.Printf("can't close http server listening on %s: %s", httpServer.Addr, err.Error())
-			}
-
-			close(errCh)
-		}()
-
-		go func() {
-			if err := httpServer.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
-				errCh <- err
-			}
-		}()
-
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case err := <-errCh:
-			return fmt.Errorf("http server can't listen and serve requests: %w", err)
-		}
-	})
+	eg.Go(httpgin.RunHTTPServerGracefully(ctx, httpServer))
 
 	if err := eg.Wait(); err != nil {
 		log.Printf("gracefully shutting down the servers: %s\n", err.Error())
 	}
-
 	log.Println("servers were successfully shutdown")
 }
