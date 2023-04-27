9d4aa6efbeb0f0dea1f322e942f3b392fcbbd4cb
TobbyMax <ageev.maxim2003@gmail.com>
2023-04-27 19:23:41 +0300

    hw9: refactor graceful shutdown 2

--- lesson9/homework/internal/ports/httpgin/server.go
+++ lesson9/homework/internal/ports/httpgin/server.go
@@ -1,6 +1,9 @@
 package httpgin
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -40,4 +43,37 @@ func NewHTTPServer(port string, a app.App) *http.Server {
 
 	AppRouter(api, a)
 	return s
+}
+
+func RunHTTPServerGracefully(ctx context.Context, server *http.Server) func() error {
+	return func() error {
+		log.Printf("starting http server, listening on %s\n", server.Addr)
+		defer log.Printf("close http server listening on %s\n", server.Addr)
+
+		errCh := make(chan error)
+
+		defer func() {
+			shCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+			defer cancel()
+
+			if err := server.Shutdown(shCtx); err != nil {
+				log.Printf("can't close http server listening on %s: %s", server.Addr, err.Error())
+			}
+
+			close(errCh)
+		}()
+
+		go func() {
+			if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+				errCh <- err
+			}
+		}()
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case err := <-errCh:
+			return fmt.Errorf("http server can't listen and serve requests: %w", err)
+		}
+	}
 }
